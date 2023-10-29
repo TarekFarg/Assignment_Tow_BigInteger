1fd22aba7a260e0fef5243e96c00ef0a084cb6a9
Mahmoud3177Khaled <124203317+Mahmoud3177Khaled@users.noreply.github.com>
2023-10-29 20:50:50 +0200

    Update A2_Task2.cpp
     == operator Overloaded

--- A2_Task2.cpp
+++ A2_Task2.cpp
@@ -1,8 +1,30 @@
 #include <iostream>
 #include <string>
 #include "BigReal.h"
+
 using namespace std;
-int main() {
-    cout << "Hello, we will solve task to here\n" << std::endl;
-    return 0;
+int main() 
+{
+    //cout << "Hello, we will solve task to here\n" << std::endl;
+    
+    string n, m;
+    cin >> n >> m;
+
+
+    BigReal a;
+    BigReal b;
+
+    a.set_num(n);
+    b.set_num(m);
+
+    //cout << a->get_sign() << endl;
+
+    if(a == b)
+        cout << "equal" << endl;
+    else
+        cout << "not equal" << endl;
+
+    
+
+
 }
