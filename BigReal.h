2b6cb325a22dbc4f3b79eab7cc22bdc5c247fdd3
Mahmoud3177Khaled <124203317+Mahmoud3177Khaled@users.noreply.github.com>
2023-10-29 23:02:33 +0200

    Update BigReal.h

--- BigReal.h
+++ BigReal.h
@@ -1,30 +1,34 @@
 //
 // Created by Tarek on 10/27/2023.
-// Modified by Mahmoud on 29/10/2023
-
+//
 #include <iostream>
 #include <string>
 using namespace std;
 
 
-class BigReal {
-private:
-    string MyNum;
-    int sign;      // -1 --> negative , 1 --> positive
-    int index_point;       // the position of the decimal point
-    void set_sign();
-    void set_len();
-    void set_index_point();
-    int len;       // length of MyNum(string)
-public:
-    void set_num(string num);
-    string get_num();
-    int get_sign();
-    BigReal();  // Default Constructor
-    BigReal(string num);   // Parameterized Constructor
+class BigReal 
+{
+    //private:
+    public:
+        string MyNum;
+        int sign;              // -1 --> negative , 1 --> positive
+        int index_point;       // the position of the decimal point
+        void set_sign();
+        void set_len();
+        void set_index_point();
+        int len;               // length of MyNum(string)
 
-    friend bool operator == (BigReal &a, BigReal &b);
+        void set_num(string num);
+        string get_num();
+        int get_sign();
+        BigReal();             // Default Constructor
+        BigReal(string num);   // Parameterized Constructor
+
+        friend bool operator == (BigReal &a, BigReal &b);
+        friend BigReal operator- (BigReal &a, BigReal &b);
     
 };
 
 bool operator == (BigReal &a, BigReal &b);
+BigReal operator- (BigReal &a, BigReal &b);
+    
