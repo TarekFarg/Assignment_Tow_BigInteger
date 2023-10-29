abfc6d004d45b8d20a77ee643d4fe683edfa8a04
Mahmoud3177Khaled <124203317+Mahmoud3177Khaled@users.noreply.github.com>
2023-10-29 20:52:21 +0200

    Update BigReal.h
    == operator overloaded

--- BigReal.h
+++ BigReal.h
@@ -1,6 +1,7 @@
 //
 // Created by Tarek on 10/27/2023.
-//
+// Modified by Mahmoud on 29/10/2023
+
 #include <iostream>
 #include <string>
 using namespace std;
@@ -10,15 +11,20 @@ class BigReal {
 private:
     string MyNum;
     int sign;      // -1 --> negative , 1 --> positive
-    int len;       // length of MyNum(string)
     int index_point;       // the position of the decimal point
     void set_sign();
     void set_len();
     void set_index_point();
+    int len;       // length of MyNum(string)
 public:
     void set_num(string num);
     string get_num();
     int get_sign();
     BigReal();  // Default Constructor
     BigReal(string num);   // Parameterized Constructor
+
+    friend bool operator == (BigReal &a, BigReal &b);
+    
 };
+
+bool operator == (BigReal &a, BigReal &b);
