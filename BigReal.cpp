668abbc5e2a61b5e133ee1b7bde6da0d35029e37
Mahmoud3177Khaled <124203317+Mahmoud3177Khaled@users.noreply.github.com>
2023-11-01 19:13:26 +0200

    Update BigReal.cpp

--- BigReal.cpp
+++ BigReal.cpp
@@ -69,15 +69,20 @@ void BigReal::set_len() {
     len = MyNum.size();
 }
 
-void BigReal::set_index_point() {
+void BigReal::set_index_point() 
+{
     string temp;
-    for (int i = 0; i < len; ++i) {
-        if (MyNum[i] == '-' || MyNum[i] == '+')
+    for (int i = 0; i < len; ++i) 
+    {   
+        if(MyNum[i] == '-')
             continue;
-        temp += MyNum[i];
+
+        if(MyNum[i] != '.')  
+            temp += MyNum[i];
+
         if (MyNum[i] == '.') {
             index_point = i;
-            for (i = i; i < len; i++) {
+            for (i = i + 1; i < len; i++) {
                 MyNump += MyNum[i];
             }    
         }
@@ -85,7 +90,7 @@ void BigReal::set_index_point() {
     MyNum = temp;
 }
 
-BigReal::BigReal() :MyNum("0")
+BigReal::BigReal() :MyNum("")
 {
     set_sign();
     set_len();
@@ -103,14 +108,166 @@ bool operator == (BigReal& a, BigReal& b)
     if (a.len != b.len)
         return 0;
 
-    if (a.MyNum == b.MyNum)
+    if (a.MyNum == b.MyNum && a.MyNump == b.MyNump && a.sign == b.sign)
         return 1;
 
     return 0;
 }
 
-
-BigReal operator- (BigReal& a, BigReal& b)
+BigReal operator+ (BigReal &a, BigReal &b)
 {
-    return BigReal();
+    BigReal c;
+    string result;
+    int remain = 0, temp = 0;
+    bool add = 0, subt = 0;
+
+
+    if(b.MyNump.length() < a.MyNump.length())
+    {
+        string filled;
+        filled += b.MyNump;
+
+        for (int i = 0; i < a.MyNump.length() - b.MyNump.length(); i++)
+            filled += '0';
+        
+        b.MyNump = filled;
+            
+    }
+
+    else if(a.MyNump.length() < b.MyNump.length())
+    {
+        string filled = "";
+        filled += a.MyNump;
+
+        for (int i = 0; i < b.MyNump.length() - a.MyNump.length(); i++)
+            filled += '0';
+
+        a.MyNump = filled;
+    }
+
+    //cout << a.MyNump << " " << b.MyNump << endl;
+
+    for (int i = a.MyNump.length() - 1; i >= 0; i--)
+    {
+        temp = (a.MyNump[i]-48) + (b.MyNump[i]-48) + remain;
+
+        if(temp >= 10)
+        {
+            temp -= 10;
+            remain = 1;
+        }
+        else
+            remain = 0;
+
+        result += (char)(temp + 48);
+
+        temp = 0;
+    }
+    for (int i = result.length() - 1; i >= 0; i--)
+    {
+        c.MyNump += result[i];
+    }
+    temp = 0; result = "";
+    if(remain == 1)
+    {
+        add = 1;
+    }
+
+    remain = 0; temp = 0; result = "";
+
+
+    if(b.MyNum.length() < a.MyNum.length())
+    {
+        string filled;
+
+        for (int i = 0; i < a.MyNum.length() - b.MyNum.length(); i++)
+            filled += '0';
+
+        filled += b.MyNum;
+
+        b.MyNum = filled;
+    }
+    else if(a.MyNum.length() < b.MyNum.length())
+    {
+        string filled = "";
+        //filled += '-';
+
+        for (int i = 0; i < b.MyNum.length() - a.MyNum.length(); i++)
+            filled += '0';
+
+        for(int i = 0; i < a.MyNum.length(); i++)
+            filled += a.MyNum[i];
+
+        a.MyNum = filled;
+    }
+
+    //cout << a.MyNum << " " << a.MyNump << endl;
+    //cout << b.MyNum << " " << b.MyNump << endl;
+
+
+    for (int i = a.MyNum.length() - 1; i >= 0; i--)
+    {
+        temp = (a.MyNum[i]-48) + (b.MyNum[i]-48) + remain;
+
+        if(temp >= 10)
+        {
+            temp -= 10;
+            remain = 1;
+        }
+        else
+            remain = 0;
+
+        result += (char)(temp + 48);
+
+        temp = 0;
+    }
+    if(remain == 1)
+        result += '1';
+    //result += '-';
+    for (int i = result.length() - 1; i >= 0; i--)
+    {
+        c.MyNum += result[i];
+    }
+    temp = 0; result = "";
+
+
+    if(add)
+    {
+        remain = 1;
+        for (int i = c.MyNum.length() - 1; i >= 0; i--)
+        {
+            temp = (c.MyNum[i]-48) + remain;
+
+            if(temp >= 10)
+            {
+                temp -= 10;
+                remain = 1;
+            }
+            else
+                remain = 0;
+
+            result += (char)(temp + 48);
+
+            temp = 0;
+        }
+        
+        if(remain == 1)
+            result += '1';
+
+        //result += '-';
+        c.MyNum = "";
+
+        for (int i = result.length() - 1; i >= 0; i--)
+            c.MyNum += result[i];
+    }
+
+    if(a.sign == 1 && b.sign == 1)
+        c.sign = 1;
+    else if(a.sign == -1 && b.sign == -1)
+        c.sign = -1;
+
+
+    //cout << c.MyNum << " " << c.MyNump << endl;
+
+    return c;
 }
