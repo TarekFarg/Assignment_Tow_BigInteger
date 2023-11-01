dc07e0ad725af51484727ce7769cd22f81d4c642
Mahmoud3177Khaled <124203317+Mahmoud3177Khaled@users.noreply.github.com>
2023-11-01 19:12:46 +0200

    Update A2_Task2.cpp

--- A2_Task2.cpp
+++ A2_Task2.cpp
@@ -3,37 +3,41 @@
 #include "BigReal.h"
 
 using namespace std;
-int main()
+int main() 
 {
     //cout << "Hello, we will solve task to here\n" << std::endl;
-
+    
     string n, m;
     cin >> n >> m;
 
 
     BigReal a(n);
     BigReal b(m);
-    //BigReal c;
+    BigReal c;
 
-    /*a.set_num("1111");
-    b.set_num("1111");
-
-    cout << a.len << " " << b.len << endl;*/
+    //cout << a.MyNum << " " << a.MyNump << " " << a.get_sign() <<  endl;    
+    //cout << b.MyNum << " " << b.MyNump << " " << b.get_sign() <<  endl; 
 
 
     /*if(a == b)
         cout << "equal" << endl;
     else
-        cout << "not equal" << endl;
-*/
+        cout << "not equal" << endl;*/
 
-    //c = a - b;
-
-    //cout << c.MyNum << endl;
-
+    
     if (a > b)
         cout << " a is bigger than b\n";
     else
         cout << "a is not bigger than b\n";
+    
+
+    /*c = a + b;
+
+    if(c.get_sign() == -1)
+        cout << '-';
+
+    cout << c.MyNum << "." << c.MyNump << endl;   */          // mynum and mynump are public for experimenting untill << is overloaded
+
+
 
 }
